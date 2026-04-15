78f6027090bd1d2f5f4bc076667ed5c4f1541c78
Márcio <marcio.magalhaes.br@gmail.com>
2026-04-15 17:56:16 -0300

    Loop para buscar os registros da tabela produtos

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 
 require 'security.php';
-
+require 'conecta.php';
 echo "usuário logado";
 
 ?>
@@ -14,6 +14,31 @@ echo "usuário logado";
 	<!--link rel="stylesheet" href="css/estilo.css" -->
 </header>
 <body>
-	<a href="logout.php">Sair</a>
+	<a href="logout.php">Sair 2</a>
+<?php	
+	$sql = "SELECT * FROM produto";
+	$stmt = $pdo->query($sql);
+	//Com loop usando while
+	echo "<h2>Lista de Clientes</h2>";
+	while ($row = $stmt->fetch()) {
+        echo "ID: " . $row['id_produto'] . "<br>";
+        echo "Descrição: " . $row['descricao'] . "<br>";
+        echo "modelo: " . $row['modelo'] . "<br>";
+        echo "gramatura: " . $row['gramatura'] . "<br>";
+		echo "cor: " . $row['cor'] . "<br>";
+		echo "preco: " . $row['preco'] . "<br>";
+        echo "-----------------------<br>";
+    }
+	//Com loop usando for
+	/*
+	$resultados = $stmt->fetchAll(); // Transforma tudo em um array
+	$total = count($resultados);    // Conta quantos registros existem
+	for ($i = 0; $i < $total; $i++) {
+		echo "ID: " . $resultados[$i]['id_produto'] . "<br>";
+        echo "Descrição: " . $resultados[$i]['descricao'] . "<br>";
+	}
+	*/
+?>
+	
 </body>
 </html>
